3434f3022663bdd17ef2829ee2aa4b7491379ef0
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-09-01 16:09:44 +0600

    Added mobentory files

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function Header() {
+  return (
+    <div id='main'>
+      <Navbar/>
+      <div className='name'>
+        <h1><span>Launch Your App</span> With Confidence And Creativity</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
