fd45da6225dfeb5c47c80fd111a8000bd9dee712
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-09-05 10:22:07 +0600

    Mobentory file added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -25,7 +25,7 @@ function Navbar() {
         <span className='nav-icon'></span>
     </label>
     <ul className='menu'>
-        <li><a href='#'>Home</a></li>
+        <li><a href='#' className='active'>Home</a></li>
         <li><a href='#'>Features</a></li>
         <li><a href='#'>About</a></li>
         <li><a href='#'>UI SS</a></li>
